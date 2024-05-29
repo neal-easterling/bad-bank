e68461abb2d4ec2bceecc265dc4d644a358fb58c
Neal Easterling <97976354+neal-easterling@users.noreply.github.com>
2024-05-29 11:08:38 -0500

    added simple navbar component

---
+++ src/Spa.jsx
@@ -0,0 +1,15 @@
+
+import NavBar from "./components/navbar";
+
+function Spa(){
+
+  return (
+    <>
+      <h1>Welcome to Bad Bank!</h1>
+      <NavBar />
+    </>
+  )
+
+}
+
+export default Spa;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+function NavBar(){
+
+  return (
+    <>
+      <a href="#">BadBank</a>
+      <a href="#/CreateAccount/">Create Account</a>
+      <a href="#/login/">Login</a>
+      <a href="#/deposit/">Deposit</a>
+      <a href="#/withdraw/">Withdraw</a>
+      <a href="#/balance/">Balance</a>
+      <a href="#/alldata/">AllData</a>
+    </>
+  );
+}
+
+export default NavBar;
