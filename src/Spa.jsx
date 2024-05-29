4596ec8cd1a5019a4275053a4b80e277a0296e17
Neal Easterling <97976354+neal-easterling@users.noreply.github.com>
2024-05-29 11:45:46 -0500

    working on routes

--- src/Spa.jsx
+++ src/Spa.jsx
@@ -1,12 +1,18 @@
 
 import NavBar from "./components/navbar";
-import HashRouter from "./components/context";
+import {HashRouter, Routes, Route, Link} from "./components/context";
+import Home from "./components/home";
 
 function Spa(){
 
   return (
     <HashRouter>
       <NavBar />
+      <Routes>
+        <Route path="/" exact component={Home} />
+      </Routes>
+      
+
     </HashRouter>
   )
 
--- src/components/context.jsx
+++ src/components/context.jsx
@@ -1,5 +1,4 @@
-import { HashRouter } from "react-router-dom";
-import { Route } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { HashRouter, Routes, Route, Link } from "react-router-dom";
 
-export default HashRouter;
+
+export {HashRouter, Routes, Route, Link};
